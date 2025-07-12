5fcfc886c7fd6be7023da312c2063ca219964130
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2025-07-13 04:35:44 +0600

    added a responsive animated navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main>
+      <Navbar></Navbar>
+    </main>
+  );
+};
+
+export default App;
