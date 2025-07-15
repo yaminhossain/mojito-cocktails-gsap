7aedcf6429d9de7bc047dd4e64a4b1bf2c850dd3
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2025-07-16 05:11:17 +0600

    started working with video in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  //Determine whether the device is mobile or not
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = SplitText.create(".title", { type: "chars, words" });
     const paragraphSplit = SplitText.create(".subtitle", { type: "lines" });
@@ -40,41 +47,78 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    //Handling Video Animation
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const videoTimeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        scrub: true,
+        pin: true,
+        start: startValue,
+        end: endValue,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      console.log("Meta data current time", videoRef.current.currentTime);
+      console.log("Meta data current time", videoRef.current.duration);
+
+      videoTimeLine.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
-    <section id="hero" className="noisy">
-      <h1 className="title">Mojito</h1>
+    <>
+      {/* =============Written Contents of Hero section================= */}
+      <section id="hero" className="noisy">
+        <h1 className="title">Mojito</h1>
 
-      <img
-        src="/images/hero-left-leaf.png"
-        alt="left leaf at hero section"
-        className="left-leaf"
-      />
-      <img
-        src="/images/hero-right-leaf.png"
-        alt="right leaf at hero section"
-        className="right-leaf"
-      />
-      <div className="body">
-        <div className="content">
-          <div className="space-y-5 hidden md:block">
-            <p>Cool. Crisp. Classic.</p>
-            <p className="subtitle">
-              Sip the Spirit <br /> of Summer
-            </p>
-          </div>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left leaf at hero section"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right leaf at hero section"
+          className="right-leaf"
+        />
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
 
-          <div className="view-cocktails">
-            <p className="subtitle">
-              Every cocktail on our menu is a blend of premium ingredients,
-              creative flair, and timeless recipes — designed to delight your
-              senses.
-            </p>
-            <a href="#cocktails">View Cocktails</a>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
           </div>
         </div>
+      </section>
+
+      {/* ====================Video Content of Hero Section================ */}
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/input.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
       </div>
-    </section>
+    </>
   );
 };
 
